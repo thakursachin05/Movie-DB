1c9c4823fe09c44a90597dfa441113bc65fdb29e
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-06-25 23:31:56 +0530

    Create MovieCard.js

---
+++ Code/src/components/MovieCard.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { addFavourite, removeFromFavourites } from '../actions';
+
+
+class MovieCard extends React.Component {
+    handleFavouriteClick = ()=>{
+        const { movie } = this.props;
+        this.props.dispatch(addFavourite(movie))
+    }
+    handleUnFavouriteClick = ()=>{
+        const {movie} = this.props;
+        this.props.dispatch(removeFromFavourites(movie));
+    };
+    render (){
+        const { movie, isFavourite } = this.props;
+        return (
+            <div className="movie-card">
+                <div className="left">
+                    <img src={movie.Poster} alt="movie-poster" />
+                </div>
+                <div className="right">
+                    <div className="title">
+                        {movie.Title}
+                    </div>
+                    <div className="plot">
+                        {movie.Plot}
+                    </div>
+                    <div className="footer">
+                        <div className="rating">
+                            {movie.imdbRating}
+                        </div>
+                        {
+                            isFavourite
+                            ? <button className="unfavourite-btn" onClick={this.handleUnFavouriteClick}> UnFavourite </button>
+                            : <button className="favourite-btn" onClick={this.handleFavouriteClick}>Favourite</button>
+                            
+                        }
+                    </div>
+                </div>
+            </div>
+          );
+    }
+  
+}
+
+export default MovieCard;
